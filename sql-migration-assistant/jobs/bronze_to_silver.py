d86588dc8799ab8b89f1a2f71da6c3c8d37cc698
robert.whiffin <robert.whiffin@databricks.com>
2025-01-03 11:22:44 +0000

    update struct name

--- sql-migration-assistant/jobs/bronze_to_silver.py
+++ sql-migration-assistant/jobs/bronze_to_silver.py
@@ -85,7 +85,7 @@ spark.sql(
     agentName STRING,
     agentResponse STRING,
     outputNotebookPath STRING,
-    similarCodeNotebooks ARRAY<STRUCT<url: STRING, intent:STRING, similarity:DOUBLE>>
+    similarCodeNotebooks ARRAY<STRUCT<notebook_url: STRING, intent:STRING, similarity:DOUBLE>>
     )
   """
 )
